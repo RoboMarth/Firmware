318af160256250057a04f375effc081da6add648
Michael Zhan <22856107+RoboMarth@users.noreply.github.com>
2018-10-27 16:01:36 -0400

    hacked sf0x driver to display data from sf30 and changed cmake to compile on newer compiler

--- cmake/common/px4_base.cmake
+++ cmake/common/px4_base.cmake
@@ -372,12 +372,12 @@ function(px4_add_common_flags)
 	set(warnings
 		-Wall
 		-Wextra
-		-Werror
+		#		-Werror
 
 		-Warray-bounds
 		-Wdisabled-optimization
 		-Wdouble-promotion
-		-Wfatal-errors
+		#		-Wfatal-errors
 		-Wfloat-equal
 		-Wformat-security
 		-Winit-self
