f20cb1a51908e19108f13c8b0ee5c3d147f1190f
Michael Zhan <22856107+RoboMarth@users.noreply.github.com>
2019-03-06 19:18:25 -0500

    Copy sf30 driver to master from 1.8.2 version
    Actually works!!!

---
+++ src/drivers/distance_sensor/sf30/sf30.cpp
@@ -0,0 +1,236 @@
+#include "sf30.h"
+
+extern "C" __EXPORT int sf30_main(int argc, char *argv[]);
+
+SF30::SF30(const char *port)
+{
+        strncpy(_port, port, sizeof(_port));
+        _port[sizeof(_port) - 1] = '\0';
+}
+
+SF30::~SF30()
+{
+        orb_unadvertise(_distance_sensor_topic);
+}
+
+int SF30::task_spawn(int argc, char *argv[])
+{
+        px4_main_t entry_point = (px4_main_t)&run_trampoline;
+        int stack_size = 1256;
+ 
+        int task_id = px4_task_spawn_cmd("sf30", SCHED_DEFAULT,
+                                         SCHED_PRIORITY_FAST_DRIVER, stack_size,
+                                         entry_point, (char *const *)argv);
+        
+        if (task_id < 0) {
+                task_id = -1;
+                return -errno;   
+        }       
+        
+        _task_id = task_id;
+                             
+        return PX4_OK; 	
+}
+
+SF30 *SF30::instantiate(int argc, char *argv[])
+{
+        SF30 *sf30 = new SF30(SF30_DEFAULT_PORT);
+        return sf30;
+}
+
+int SF30::custom_command(int argc, char *argv[])
+{
+        return print_usage("Unrecognized command.");
+}
+
+int SF30::print_usage(const char *reason)
+{
+	return PX4_OK;
+}
+
+void SF30::run()
+{
+	open_serial();
+
+        struct distance_sensor_s report = {};
+        _distance_sensor_topic = orb_advertise(ORB_ID(distance_sensor), &report);
+        
+        if (_distance_sensor_topic == nullptr) {
+                PX4_WARN("Advertise failed.");
+		close_serial();
+                return;     
+        }
+
+	tcflush(_fd, TCIFLUSH);
+	uint64_t start_time = hrt_absolute_time();	
+
+	while (!should_exit()) {
+		
+		if (read_most_recent_bytes() == PX4_OK) {
+
+			float distance = (_high_byte & 0x7F) + (_low_byte / 100.0f);
+
+			report.timestamp = _report_timestamp;
+			report.type = distance_sensor_s::MAV_DISTANCE_SENSOR_LASER;
+			report.orientation = distance_sensor_s::ROTATION_DOWNWARD_FACING;
+			report.current_distance = distance;
+			report.min_distance = 0.2f;
+			report.max_distance = 100.0f;
+
+			orb_publish(ORB_ID(distance_sensor), _distance_sensor_topic, &report);
+		}
+
+		uint64_t loop_time = hrt_absolute_time() - start_time;
+		uint32_t sleep_time = (loop_time > OUTPUT_INTERVAL_US) ? 0 : OUTPUT_INTERVAL_US - loop_time;
+		PX4_ERR("%d", sleep_time);
+		usleep(sleep_time);
+				
+		start_time = hrt_absolute_time();	
+	}
+
+	PX4_INFO("Exiting.");
+	close_serial();
+}
+
+int SF30::read_most_recent_bytes()
+{
+	uint8_t bytes_buffer[2];
+	int ret = ::read(_fd, bytes_buffer, sizeof(bytes_buffer));
+	int err = errno;
+
+	_report_timestamp = hrt_absolute_time(); // most accurate place to get timestamp?
+
+	uint8_t temp_buffer[2];
+	int ret2 = ::read(_fd, temp_buffer, sizeof(temp_buffer));
+	int err2 = errno;
+
+	// if the bytes read are the last two bytes available
+	if (ret == 2 && ret2 == -1 && err2 == 11) { 
+		
+		// sanity check: if the bytes are correctly formatted
+		if (is_high_byte(bytes_buffer[0]) && !is_high_byte(bytes_buffer[1])) {
+			_high_byte = bytes_buffer[0];
+			_low_byte = bytes_buffer[1];
+			return PX4_OK;
+
+		} else {
+
+			PX4_ERR("sanity fail, not publishing");
+			return PX4_ERROR;
+		}
+		
+	// something's wrong
+	} else { 
+		// tried to read too early
+		if (ret == 1 || ret == 0 || (ret == -1 && err == 11)) { 
+
+			PX4_ERR("too early, ret: %d, ret2: %d", ret, ret2);
+
+		// tried to read too late
+		} else if (ret == 2 && ret2 >= 0) {
+
+			PX4_ERR("too late, ret2: %d", ret2);
+
+		// unexpected error in ::read system call
+		} else if (ret == -1 && err != 11) { 
+
+			tcflush(_fd, TCIFLUSH);
+			PX4_ERR("read error: %d, errno: %d, buffer flushed", ret, err);
+
+		// uncaught result
+		} else {
+			PX4_ERR("unexpected result");
+		}
+		return PX4_ERROR;
+	}
+}
+
+bool SF30::is_high_byte(uint8_t byte)
+{
+	return (0b10000000 & byte) ==
+	        0b10000000;
+}
+
+int SF30::open_serial()
+{
+        _fd = px4_open(_port, O_RDWR | O_NOCTTY | O_NONBLOCK);
+        
+        if (_fd < 0) {
+                PX4_WARN("Failed to open serial port");
+                return PX4_ERROR;
+        }
+        
+        struct termios uart_config;
+        
+        int termios_state;
+        
+        // Fill the struct for the new configuration.
+        tcgetattr(_fd, &uart_config);
+        
+        // Input flags - Turn off input processing:
+        // convert break to null byte, no CR to NL translation,
+        // no NL to CR translation, don't mark parity errors or breaks
+        // no input parity check, don't strip high bit off,
+        // no XON/XOFF software flow control
+        //
+        uart_config.c_iflag &= ~(IGNBRK | BRKINT | ICRNL |  INLCR | IGNCR | PARMRK | INPCK | ISTRIP | IXON | IXOFF);
+        
+        uart_config.c_iflag |= IGNPAR;
+        
+        // Output flags - Turn off output processing:
+        // no CR to NL translation, no NL to CR-NL translation,
+        // no NL to CR translation, no column 0 CR suppression,
+        // no Ctrl-D suppression, no fill characters, no case mapping,
+        // no local output processing
+        uart_config.c_oflag &= ~(OCRNL | ONLCR | ONLRET | ONOCR | OFILL | OPOST);
+        
+        // No line processing:
+        // echo off, echo newline off, canonical mode off,
+        // extended input processing off, signal chars off
+        uart_config.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
+        
+        // No parity, one stop bit, disable flow control.
+        uart_config.c_cflag &= ~(CSIZE | PARENB | CSTOPB | CRTSCTS);
+        
+        uart_config.c_cflag |= (CS8 | CREAD | CLOCAL);
+        
+        uart_config.c_cc[VMIN] = 1;
+        
+        uart_config.c_cc[VTIME] = 0;
+        
+        unsigned speed = BAUD_RATE;
+        
+        // Set the baud rate.
+        if ((termios_state = cfsetispeed(&uart_config, speed)) < 0) {
+                PX4_WARN("ERR CFG: %d ISPD", termios_state);
+                return PX4_ERROR;
+        }
+        
+        if ((termios_state = cfsetospeed(&uart_config, speed)) < 0) {
+                PX4_WARN("ERR CFG: %d OSPD\n", termios_state);
+                return PX4_ERROR;
+        }
+        
+        if ((termios_state = tcsetattr(_fd, TCSANOW, &uart_config)) < 0) {
+                PX4_WARN("ERR baud %d ATTR", termios_state);
+                return PX4_ERROR;
+        }
+        
+        return _fd;
+}
+
+int SF30::close_serial()
+{
+        int ret = px4_close(_fd);
+          
+        if (ret != 0) {
+                PX4_WARN("Could not close serial port");
+        }
+
+        return ret;
+}
+        
+int sf30_main(int argc, char *argv[])
+{
+	return SF30::main(argc, argv);
+}
---
+++ src/drivers/distance_sensor/sf30/sf30.h
@@ -0,0 +1,55 @@
+#include <cstring>
+#include <termios.h>
+
+#include <drivers/device/device.h>
+#include <drivers/drv_hrt.h>
+
+#include <uORB/topics/distance_sensor.h>
+
+#include <px4_module.h>
+#include <px4_module_params.h>
+#include <px4_tasks.h>
+
+#define SF30_DEFAULT_PORT 	"/dev/ttyS2"
+#define BAUD_RATE 		B9600
+#define OUTPUT_INTERVAL_US 	25588
+
+class SF30 : public ModuleBase<SF30>
+{
+public:
+	SF30(const char *port = SF30_DEFAULT_PORT);
+
+	virtual ~SF30();
+
+	static int task_spawn(int argc, char *argv[]);
+
+	static SF30 *instantiate(int argc, char *argv[]);
+
+	static int custom_command(int argc, char *argv[]);
+
+	static int print_usage(const char *reason = nullptr);
+
+	void run() override;
+
+private:
+	// returns PX4_OK if bytes are publishable, PX4_ERROR if not
+	int read_most_recent_bytes();
+
+	bool is_high_byte(uint8_t byte);
+
+	int open_serial();
+
+	int close_serial();
+
+	int _fd = -1;
+
+	char _port[20];
+
+	orb_advert_t _distance_sensor_topic = nullptr;
+
+	uint8_t _high_byte = 0;
+
+	uint8_t _low_byte = 0;
+
+	uint64_t _report_timestamp = 0;
+};
